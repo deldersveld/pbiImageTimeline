15359c97f6036a9dc5420e789f398727f3a40f18
David Eldersveld <deldersveld@blue-granite.com>
2017-10-07 00:49:09 -0400

    packaged

--- .tmp/drop/visual.js
+++ .tmp/drop/visual.js
@@ -1244,8 +1244,8 @@ var powerbi;
     (function (visuals) {
         var plugins;
         (function (plugins) {
-            plugins.timeline1E0B9DD0A83A4E79BB5F9DE15C7690AE_DEBUG = {
-                name: 'timeline1E0B9DD0A83A4E79BB5F9DE15C7690AE_DEBUG',
+            plugins.timeline1E0B9DD0A83A4E79BB5F9DE15C7690AE = {
+                name: 'timeline1E0B9DD0A83A4E79BB5F9DE15C7690AE',
                 displayName: 'Image Timeline',
                 class: 'Visual',
                 version: '1.1.0',
